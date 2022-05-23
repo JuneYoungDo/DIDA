f3de6af7be5d797116b4abc24cc311ebd4559b3c
juneyoungdo <ehwnsdud1004@naver.com>
2022-05-24 00:47:55 +0900

    [DIDA_06]Manage App Version

---
+++ src/main/java/com/service/dida/Utils/appManage/DTO/GetAppVersionDto.java
@@ -0,0 +1,11 @@
+package com.service.dida.Utils.appManage.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class GetAppVersionDto {
+    private Long version;
+}
